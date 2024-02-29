6472d5351a5ca352c6317af54b0ff35c062d5437
Josip Maretić <josip.maretic2011@outlook.com>
2024-02-29 19:25:27 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<style>
+	.container {
+		font-family: Arial, Helvetica, sans-serif;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: cadetblue;
+	}
+	.heading {
+		color: red;
+	}
+</style>
+
+<div class="container">
+	<h1 class="heading">Tauri app test</h1>
+</div>
