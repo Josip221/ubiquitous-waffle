1e3fcff647245e7eaf5fee1cf9796ea9a05bdfff
Josip Maretić <josip.maretic2011@outlook.com>
2024-02-29 21:08:16 +0100

    File reading

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,64 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs;
+use serde::Serialize;
+
+
+
+#[derive(Debug, Serialize)]
+struct DirInfo {
+    name: String,
+    size: u64,
+    is_dir: bool,
+    is_file: bool,
+    created: u64,
+    modified: u64,
+    // permissions: u32,
+
+}
 
 #[tauri::command]
-fn test_function(message: String) {
-  println!("Hello from test function: {}", message);
+fn get_current_dir() -> Vec<DirInfo> {
+   let current_dir = std::env::current_dir().expect("Failed to get current directory");
+
+     let mut dir_info_vec: Vec<DirInfo> = Vec::new();
+
+    // List contents of the current directory
+    if let Ok(entries) = fs::read_dir(&current_dir) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                let metadata = fs::metadata(&path).expect("Failed to get metadata");
+
+                let dir_info = DirInfo {
+                    name: path.file_name().unwrap().to_string_lossy().into_owned(),
+                    size: metadata.len(),
+                    is_dir: metadata.is_dir(),
+                    is_file: metadata.is_file(),
+                    created: metadata.created().unwrap().elapsed().unwrap().as_secs(),
+                    modified: metadata.modified().unwrap().elapsed().unwrap().as_secs(),
+                    // permissions: metadata.permissions().mode(),
+                };
+
+                dir_info_vec.push(dir_info);
+            }
+        }
+    } else {
+        println!("Failed to read directory contents");
+    }
+    dir_info_vec
 }
 
 
 fn main() {
+  // let menu = Menu::new();
+    // .add_native_item(MenuItem::Quit);
+
+
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![test_function])
+    // .menu(menu)
+    .invoke_handler(tauri::generate_handler![get_current_dir])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,24 @@
-<script>
+<script lang="ts">
 	import { invoke } from '@tauri-apps/api/tauri'
+
+	interface Dir {
+		name: string
+		size: number
+		isDir: boolean
+		isFile: boolean
+		created: number
+		modified: number
+	}
+
+	let dir_list: Dir[] = []
+
+	const handleClick = () => {
+		invoke('get_current_dir', {message: "hello from backend"}).then((res: any) => {
+			dir_list = [...dir_list, ...res]
+		})
+	}
+
+	$: console.log(dir_list)
 </script>
 
 
@@ -16,9 +35,19 @@
 	.heading {
 		color: white;
 	}
+
+	.folder--item {
+		color: white;
+	}
 </style>
 
 <div class="container">
-	<h1 class="heading">Tauri app test</h1>
-	<button on:click={() => invoke('test_function', {message: "hello from backend"})}>Invoke</button>
+	<h1 class="heading">Tauri File Explorer</h1>
+	<button on:click={handleClick}>Invoke</button>
+	{#each dir_list as dir}
+		<div class="folder--item">
+			<span>{dir.name}</span>
+			<span>{dir.size}</span>
+		</div>
+	{/each}
 </div>
